00e03814bdd70a13b8fd2a3804a11c8a1dbd1ba7
pnevmat <pnevmat001@gmail.com>
2022-10-31 14:22:13 +0200

    fixes styles of icons and names

--- src/App.js
+++ src/App.js
@@ -5,26 +5,26 @@ import './App.css';
 
 function App() {
   return (
-    <div className="m-auto max-w-7xl text-center ">
-      <div>
-        <h2>Icons Solid</h2>
+    <div className="flex items-center justify-between w-full max-w-7xl py-48 m-auto text-center ">
+      <div className="w-2/5">
+        <h2 className="mb-12 font-bold text-lg text-gray-900">Icons Solid</h2>
         {Object.keys(iconsSolid).map((icon, i) => (
-          <div className="mb-6" key={i}>
-            <span className="mb-2 text-gray-100">{icon}</span>
-            <div className="w-6 h-6 text-gray-100">
+          <div className="flex items-center justify-between mb-6" key={i}>
+            <div className="w-6 h-6 text-gray-700">
               {iconsSolid[icon].render(icon)}
             </div>
+            <span className="mb-2 text-gray-700">{icon}</span>
           </div>
         ))}
       </div>
-      <div>
-        <h2>Icons Outline</h2>
+      <div className="w-2/5">
+        <h2 className="mb-12 font-bold text-lg text-gray-900">Icons Outline</h2>
         {Object.keys(iconsOutline).map((icon, i) => (
-          <div className="mb-6" key={i}>
-            <span className="mb-2 text-gray-100">{icon}</span>
-            <div className="w-6 h-6 text-gray-100">
+          <div className="flex items-center justify-between mb-6" key={i}>
+            <div className="w-6 h-6 text-gray-700">
               {iconsOutline[icon].render(icon)}
             </div>
+            <span className="mb-2 text-gray-700">{icon}</span>
           </div>
         ))}
       </div>
